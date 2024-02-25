a471c20bda56895aeaf804995bfa3a5bed305c70
charlie <ddd1331@gmail.com>
2024-02-25 23:14:50 +0900

    fix: PostViewResponse 경로 수정

--- src/post/repository/post.repositoyr.ts
+++ src/post/repository/post.repositoyr.ts
@@ -1,6 +1,6 @@
 import { Repository } from 'typeorm';
+import { PostViewResponse } from '../dto/post-view.response';
 import { Post } from '../entities/post.entity';
-import { PostViewResponse } from './PostViewResponse';
 
 export interface PostRepository extends Repository<Post> {
   getPostView(postId: number): Promise<PostViewResponse>;
